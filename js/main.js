c60163084b44129fdcec09aa400a3c6e0a7fff16
depadavid <100303655+depadavid@users.noreply.github.com>
2024-05-08 16:07:24 -0500

    feat: :construction: Corrección carpetas y archivos
    Se corrige la ubicación de algunos archivos y carpetas

---
+++ js/main.js
@@ -0,0 +1,18 @@
+import { 
+    getAllOfficesCodeAndCity, 
+    getAllOfficesFromSpainCityAndMovil 
+} from "./module/offices.js";
+
+import { 
+    getAllEmployeesWithBossAndCodeSeven,
+    getBossFullNameAndEmail,
+    getAllEmployeesNotSalesReps,
+    getAll3,
+    getAll
+} from "./module/employees.js";
+import { 
+    getAll2,
+    getClientsEmploy
+} from "./module/clients.js";
+
+console.log(await getAll());

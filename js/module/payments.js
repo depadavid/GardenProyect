0cf07de18fddf442c2fe3f83ed166bf6279a0f57
depadavid <100303655+depadavid@users.noreply.github.com>
2024-05-09 15:03:00 -0500

    feat: :construction: Get pays clients greater than
    8. Returns a list with the customer code of those customers who made a payment in 2008. Keep in mind that you must eliminate those customer codes that appear repeated. Solve the query:

--- js/module/payments.js
+++ js/module/payments.js
@@ -4,3 +4,21 @@ export default async function getPagos() {
     const response = await fetch("http://localhost:5502/payments")
     return await response.json()
 };
+
+// 8. Devuelve un listado con el código de cliente de aquellos clientes que realizaron algún pago en 2008. Tenga en cuenta que deberá eliminar aquellos códigos de cliente que aparezcan repetidos. Resuelva la consulta:
+
+async function getClientesPagosMayorA2008() {
+    const pagos = await getPagos()
+    const pagosEn2008 = []
+    pagos.forEach(pago => {
+        let { code_client, date_payment } = pago
+        if (date_payment.split('-')[0] == "2008") {
+            if (!pagosEn2008.includes(code_client)) {
+                pagosEn2008.push(code_client)
+            }
+        }
+    })
+    console.log(pagosEn2008);
+};
+
+// getClientesPagosMayorA2008()
